6888c4d22b076f41e2d7405e1983a202010c9735
Victor Villazon <victorzn23@gmail.com>
2023-07-08 11:55:44 -0500

    recuperando empresa en el formulario

--- src/App.vue
+++ src/App.vue
@@ -10,30 +10,39 @@
   }
 
   const enterprises = ref(empresas)
-  const formData = ref(DEFAULT_FORM_DATA)
+  const formData = ref({ ...DEFAULT_FORM_DATA })
 
   const onSubmit = () => {
     const { id, name, owner } = formData.value
 
-    const newId = crypto.randomUUID()
-    const newEnterprise = {
-      id: newId,
-      name,
-      owner
+    const isNew = !formData.value.id
+
+    if (isNew) { // Is New
+      const newId = crypto.randomUUID()
+      const newEnterprise = {
+        id: newId,
+        name,
+        owner
+      }
+
+      enterprises.value = [ ...enterprises.value, newEnterprise ]
+
+      console.log('guardando data....', newEnterprise)
+    } else { // Update
+      console.log('>>> guardando la actualización...')
     }
-
-    enterprises.value = [
-      ...enterprises.value,
-      newEnterprise
-    ]
-
-    console.log('guardando data....', newEnterprise)
 
     formData.value = {
       id: null,
       name: '',
       owner: ''
     }
+  }
+
+  const onUpdate = (enterprise) => {
+    formData.value = {...enterprise}
+
+    console.log('actualizando empresa....', enterprise.name)
   }
 </script>
 
@@ -58,6 +67,7 @@
             <th>#</th>
             <th>Name</th>
             <th>Owner</th>
+            <th>Actions</th>
           </tr>
         </thead>
         <tbody>
@@ -65,12 +75,17 @@
             <td>{{ index + 1 }}</td>
             <td>{{ empresa.name }}</td>
             <td>{{ empresa.owner }}</td>
+            <td>
+              <button @click="onUpdate(empresa)">
+                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" style="fill:#fff"><path d="m16 2.012 3 3L16.713 7.3l-3-3zM4 14v3h3l8.299-8.287-3-3zm0 6h16v2H4z"/></svg>  
+              </button>
+            </td>
           </tr>
         </tbody>
         </table>
       </div>
       <div>
-        <!-- {{ formData }} -->
+        {{ formData }}
         <form @submit.prevent="onSubmit">
           <label>
             Nombre empresa
