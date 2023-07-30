3eb37d211bbb090b97fe05789d703a76ab5ab2d3
Victor Villazon <victorzn23@gmail.com>
2023-07-30 09:54:40 -0500

    semana04, dia02, vue router con login y register

--- src/views/RegisterView.vue
+++ src/views/RegisterView.vue
@@ -1,3 +1,54 @@
 <template>
-  REGISTER VIEW
+  <div class="container">
+
+    <article class="grid mt-100">
+      <div></div>
+      <div>
+        <hgroup>
+          <h1>Register</h1>
+          <h2>Ingresa tus datos para registrarte</h2>
+        </hgroup>
+
+        <form>
+          <BaseInput
+            label="Fullname"
+            type="text"
+            name="fullname"
+            placeholder="Fullname"
+            required
+          />
+
+          <BaseInput
+            label="Email"
+            type="email"
+            name="email"
+            placeholder="Email"
+            required
+          />
+          
+          <BaseInput
+            label="Password"
+            type="password"
+            name="password"
+            placeholder="Password"
+            required
+          />
+
+          <BaseButton
+            type="submit"
+            class="contrast"
+            label="Register"
+          />
+        </form>
+      </div>
+      <div></div>
+    </article>
+
+  </div>
 </template>
+
+<style scoped>
+.mt-100 {
+  margin-top: 100px;
+}
+</style>
