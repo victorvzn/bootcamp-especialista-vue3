21de481efb104ecfabba3ef218f49f935a03b996
Victor Villazon <victorzn23@gmail.com>
2023-08-05 10:17:41 -0500

    semana05, dia01, agregando maquetación del header

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,7 +2,22 @@
 </script>
 
 <template>
-  <main>
-    <h1>Invoices view</h1>
+  <header class="w-[940px] mx-auto flex justify-between items-center my-20">
+    <div class="text-white">
+      <h1 class="text-4xl font-extrabold">Invoices</h1>
+      <span class="text-sm">There are 7 total invoices</span>
+    </div>
+
+    <button
+      class="bg-[#7c5df9] rounded-full px-5 py-3 font-semibold text-white"
+    >
+      New Invoice
+    </button>
+  </header>
+
+  <main class="w-[940px] bg-red-300 mx-auto">
+    <article class="bg-[#1f213a]">
+      
+    </article>
   </main>
 </template>
