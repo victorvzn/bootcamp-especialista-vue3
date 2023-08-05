474e93d89db2fc203d878feae72eba5b12107a4c
Victor Villazon <victorzn23@gmail.com>
2023-08-05 09:52:18 -0500

    semana05, dia01, agregando maquetación inicial de la vista invoices

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,9 @@ import { RouterView } from 'vue-router'
 </script>
 
 <template>
-  <h1 class="text-4xl text-orange-400">Vue + Pinia</h1>
-  
+  <nav>
+    Invoice app
+  </nav>
+
   <RouterView />
 </template>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,8 @@
 <script setup>
-import TheWelcome from '../components/TheWelcome.vue'
 </script>
 
 <template>
   <main>
-    <TheWelcome />
+    <h1>Invoices view</h1>
   </main>
 </template>
