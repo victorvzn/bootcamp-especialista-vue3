2c62e658a28e97b51af6ff3f0a80fc0b9f24d84a
Victor Villazon <victorzn23@gmail.com>
2023-08-05 11:47:13 -0500

    semana05, dia01, refactor de componentes de la vista home

---
+++ src/components/home-view/InvoicesList.vue
@@ -0,0 +1,14 @@
+<script setup>
+  import { useInvoicesStore } from '@/stores/invoices'
+
+  const { getInvoices } = useInvoicesStore()
+</script>
+
+<template>
+  <article
+    class="bg-[#1f213a] text-white"
+    v-for="invoice in getInvoices()"
+  >
+    {{ invoice.code }}
+  </article>
+</template>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,35 +1,20 @@
 <script setup>
   import { onBeforeMount } from 'vue'
 
+  import HomeViewHeader from '@/components/home-view/HomeViewHeader.vue'
+  import InvoiceList from '@/components/home-view/InvoicesList.vue'
+
   import { useInvoicesStore } from '@/stores/invoices'
 
-  const { fetchAll, getInvoices } = useInvoicesStore()
+  const { fetchAll } = useInvoicesStore()
 
-  onBeforeMount(async () => {
-    await fetchAll()
-  })
+  onBeforeMount(async () => await fetchAll())
 </script>
 
 <template>
-  <header class="w-[940px] mx-auto flex justify-between items-center my-20">
-    <div class="text-white">
-      <h1 class="text-4xl font-extrabold">Invoices</h1>
-      <span class="text-sm">There are 7 total invoices</span>
-    </div>
-
-    <button
-      class="bg-[#7c5df9] rounded-full px-5 py-3 font-semibold text-white"
-    >
-      New Invoice
-    </button>
-  </header>
+  <HomeViewHeader />
 
   <main class="w-[940px] mx-auto flex flex-col gap-5">
-    <article
-      class="bg-[#1f213a] text-white"
-      v-for="invoice in getInvoices()"
-    >
-      {{ invoice.code }}
-    </article>
+    <InvoiceList />
   </main>
 </template>
