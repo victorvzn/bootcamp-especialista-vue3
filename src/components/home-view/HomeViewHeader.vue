887baa32ccb0e555b2baadc7588b890694e77620
Victor Villazon <victorzn23@gmail.com>
2023-08-06 10:14:44 -0500

    semana05, dia02, convirtiendo button a un componente base

--- src/components/home-view/HomeViewHeader.vue
+++ src/components/home-view/HomeViewHeader.vue
@@ -1,6 +1,8 @@
 <script setup>
 import { useInvoicesStore } from '@/stores/invoices'
 
+import BaseButton from '@/components/shared/BaseButton.vue'
+
 const { getTotalInvoices } = useInvoicesStore()
 </script>
 
@@ -12,12 +14,7 @@ const { getTotalInvoices } = useInvoicesStore()
     </div>
 
     <RouterLink :to="{ name: 'invoice-new' }">
-      <button
-        class="bg-[#7c5df9] rounded-full px-5 py-3 font-semibold text-white flex items-center gap-4"
-      >
-        <font-awesome-icon icon="fa-solid fa-circle-plus" size="2x" />
-        New Invoice
-      </button>
+      <BaseButton />
     </RouterLink>
   </header>
 </template>
---
+++ src/components/shared/BaseButton.vue
@@ -0,0 +1,8 @@
+<template>
+  <button
+    class="bg-[#7c5df9] rounded-full px-5 py-3 font-semibold text-white flex items-center gap-4"
+  >
+    <font-awesome-icon icon="fa-solid fa-circle-plus" size="2x" />
+    New Invoice
+  </button>
+</template>
