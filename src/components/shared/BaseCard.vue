0d2766bc5513c591f5bf64ebbb8fe626524851c2
Victor Villazon <victorzn23@gmail.com>
2023-08-19 10:40:26 -0500

    añadiendo maquetación del listado de cards

---
+++ src/components/shared/BaseCard.vue
@@ -0,0 +1,13 @@
+<script setup>
+</script>
+
+<template>
+  <v-card width="300">
+    <template v-slot:title>
+      Build UI for onboarding flow
+    </template>
+    <template v-slot:subtitle>
+      0 of 3 subtasks
+    </template>
+  </v-card>
+</template>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,7 +1,62 @@
 <script setup>
-  
+import BaseCard from '@/components/shared/BaseCard.vue'
 </script>
 
 <template>
-  <h1>Home page</h1>
+  <div class="d-flex" style="gap:2rem;">
+    <VSheet class="d-flex flex-column" style="gap:1rem;">
+      <h2 class="text-subtitle-2 flex">
+        <v-badge
+          dot
+          color="error"
+          inline
+        ></v-badge>
+        TODO (4)
+      </h2>
+    
+      <BaseCard />
+      <BaseCard />
+      <BaseCard />
+      <BaseCard />
+      <BaseCard />
+      <BaseCard />
+    </VSheet>
+    <VSheet class="d-flex flex-column" style="gap:1rem;">
+      <h2 class="text-subtitle-2 flex">
+        <v-badge
+          dot
+          color="error"
+          inline
+        ></v-badge>
+        DOING (4)
+      </h2>
+
+      <BaseCard />
+      <BaseCard />
+      <BaseCard />
+      <BaseCard />
+      <BaseCard />
+      <BaseCard />
+    </VSheet>
+    <VSheet class="d-flex flex-column" style="gap:1rem;">
+      <h2 class="text-subtitle-2 flex">
+        <v-badge
+          dot
+          color="error"
+          inline
+        ></v-badge>
+        DONE (4)
+      </h2>
+
+      <BaseCard />
+      <BaseCard />
+      <BaseCard />
+      <BaseCard />
+      <BaseCard />
+      <BaseCard />
+    </VSheet>
+    <VSheet color="grey-lighten-4 pa-8 text-h6 d-flex justify-center align-center" width="300" rounded>
+      + New Column
+    </VSheet>
+  </div>
 </template>
