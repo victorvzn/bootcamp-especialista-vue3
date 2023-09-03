cde4b8bc59bd02bf3759ac9ec64df8c48fc24b67
Victor Villazon <victorzn23@gmail.com>
2023-09-03 11:09:49 -0500

    configurando variables de entorno para nuestro proyecto

--- src/services/config.js
+++ src/services/config.js
@@ -1,8 +1,10 @@
+console.log('.ENV', import.meta.env.VITE_PROJECT_ID)
+
 export const firebaseConfig = {
-  apiKey: "AIzaSyDYC8UGNKjIbOwWj7HmKc7af4Z9siDxAhs",
-  authDomain: "task-managment-victor.firebaseapp.com",
-  projectId: "task-managment-victor",
-  storageBucket: "task-managment-victor.appspot.com",
-  messagingSenderId: "613364196089",
-  appId: "1:613364196089:web:1c4e33c874d0d6e6c5b828"
+  apiKey: import.meta.env.VITE_API_KEY,
+  authDomain: import.meta.env.VITE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_APP_ID
 }
