ae9367f187504f80dcd0640b22fa911ee8405b1a
Victor Villazon <victorzn23@gmail.com>
2023-09-03 10:14:27 -0500

    agregando boton de eliminación en cada items de la lista de boards

--- src/components/boards/BoardList.vue
+++ src/components/boards/BoardList.vue
@@ -59,48 +59,12 @@ const handleRemoveBoard = () => {
           link
           :to="{ name: 'board-detail', params: { id: board.id }}"
         >
-          <template v-slot:append>
-            <v-dialog
-              v-model="showRemoveBoardModal"
-              persistent
-              width="auto"
-            >
-              <template v-slot:activator="{ props }">
-                <v-btn
-                  v-bind="props"
-                  color="grey-lighten-1"
-                  variant="text"
-                  @click="handleShowRemoveBoardModal"
-                >✖</v-btn>
-              </template>
-              <v-card>
-                <v-card-title class="text-h5">
-                  Remove board
-                </v-card-title>
-                <v-card-text>
-                  Are you sure you want to delete this board?
-
-                  This will delete this board pemanently. You cannot undo this action.
-                </v-card-text>
-                <v-card-actions>
-                  <v-spacer></v-spacer>
-                  <v-btn
-                    color="green-darken-1"
-                    variant="text"
-                    @click="showRemoveBoardModal = false"
-                  >
-                    No
-                  </v-btn>
-                  <v-btn
-                    color="green-darken-1"
-                    variant="text"
-                    @click="handleRemoveBoard"
-                  >
-                    Yes
-                  </v-btn>
-                </v-card-actions>
-              </v-card>
-            </v-dialog>
+           <template v-slot:append>
+            <v-btn
+              color="grey-lighten-1"
+              variant="text"
+              @click="handleShowRemoveBoardModal"
+            >✖</v-btn>
           </template>
         </v-list-item>
     </v-list>
@@ -166,6 +130,48 @@ const handleRemoveBoard = () => {
           </v-btn>
         </v-form>
       </v-card-text>
+    </v-card>
+  </v-dialog>
+
+  <v-dialog
+    v-model="showRemoveBoardModal"
+    persistent
+    width="auto"
+  >
+    <template v-slot:activator="{ props }">
+      <v-btn
+        v-bind="props"
+        color="grey-lighten-1"
+        variant="text"
+        @click="handleShowRemoveBoardModal"
+      >✖ Delete2</v-btn>
+    </template>
+    <v-card>
+      <v-card-title class="text-h5">
+        Remove board
+      </v-card-title>
+      <v-card-text>
+        Are you sure you want to delete this board?
+
+        This will delete this board pemanently. You cannot undo this action.
+      </v-card-text>
+      <v-card-actions>
+        <v-spacer></v-spacer>
+        <v-btn
+          color="green-darken-1"
+          variant="text"
+          @click="showRemoveBoardModal = false"
+        >
+          No
+        </v-btn>
+        <v-btn
+          color="green-darken-1"
+          variant="text"
+          @click="handleRemoveBoard"
+        >
+          Yes
+        </v-btn>
+      </v-card-actions>
     </v-card>
   </v-dialog>
 </template>
